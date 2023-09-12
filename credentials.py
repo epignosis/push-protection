1584bc03417763215c24a8ee07dd7d05b9a67e77
= <=>
2023-09-12 14:46:32 +0300

    Test detection of aws creds.

---
+++ credentials.py
@@ -0,0 +1,3 @@
+awsAccessKeyId = 'AKIAFAKEID1234567890'
+awsSecretAccessKey = '0aFakeSecretKey1234567890abcD1234567890'
+awsSessionToken = 'FQoFakeSessionToken1234567890...<remainder of fake security token>'
